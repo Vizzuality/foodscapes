496022114db1fdb9da160b796493101ad1f627e3
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-01-23 11:44:58 +0100

    Comlink

--- client/src/containers/home/component.tsx
+++ client/src/containers/home/component.tsx
@@ -1,9 +1,14 @@
+import dynamic from 'next/dynamic';
+
 import Hero from './hero';
+
+const Data = dynamic(() => import('./data'), { ssr: false });
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Data />
     </>
   );
 };
---
+++ client/src/workers/sql.ts
@@ -0,0 +1,24 @@
+import { expose } from 'comlink';
+
+export class MyClass {
+  private _counter: number;
+
+  constructor(init: number) {
+    this._counter = init;
+  }
+
+  get counter() {
+    return this._counter;
+  }
+
+  increment(delta = 1) {
+    this._counter += delta;
+  }
+}
+
+expose(MyClass);
+
+export interface MyClass {
+  counter: number;
+  increment(delta?: number): void;
+}
