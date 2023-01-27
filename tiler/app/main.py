763e7832da754784d6e54ba0333c8112be6f6b49
Biel Stela <biel.stela@gmail.com>
2023-01-27 16:57:30 +0100

    Add default cog url for the tiler according to env var

--- tiler/app/main.py
+++ tiler/app/main.py
@@ -5,17 +5,24 @@ from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 
 from titiler.core.middleware import TotalTimeMiddleware, LoggerMiddleware
 
-root_path = getenv("TILER_ROOT_PATH", "")
+ROOT_PATH = getenv("TILER_ROOT_PATH", "")
+COG_PATH = getenv("TILER_FOODSCAPES_COG_FILENAME", )
 
-app = FastAPI(title="Tiler!", root_path=root_path)
+
+def default_cog_url() -> str:
+    return COG_PATH
+
+
+app = FastAPI(title="Tiler!", root_path=ROOT_PATH)
 app.add_middleware(TotalTimeMiddleware)
 app.add_middleware(LoggerMiddleware)
 
 # single COG tiler. One file can have multiple bands
-cog = TilerFactory(router_prefix="/cog")
+cog = TilerFactory(router_prefix="/cog", path_dependency=default_cog_url)
 app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"], prefix="/cog")
 
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
 
 @app.get("/healthz", description="Health Check", tags=["Health Check"])
 def ping():
