1007952b9dd90c3b489debb36cc5542f8013bd57
Maria Sola <maria.sola@vizzuality.com>
2023-02-01 11:56:28 +0100

    transition scroll and animation

--- client/src/containers/home/layers/component.tsx
+++ client/src/containers/home/layers/component.tsx
@@ -6,6 +6,8 @@ import { AnimatePresence, motion } from 'framer-motion';
 
 import useImagesPreloader from 'hooks/images-preloader';
 
+import { STEP_DURATION } from 'containers/home/animations/constants';
+import FadeY from 'containers/home/animations/fadeY';
 import Wrapper from 'containers/wrapper';
 
 import Icon from 'components/icon';
@@ -48,111 +50,122 @@ const Layers = () => {
   );
 
   return (
-    <Wrapper>
-      <div className="grid h-small-screen grid-cols-12 gap-20">
-        <div
-          className={cn({
-            'relative col-span-5': true,
-            'before:absolute before:top-0 before:right-full before:h-full before:w-[50vw]': true,
-            [LAYER.backgroundColor]: true,
-          })}
-        >
-          <AnimatePresence>
-            <motion.div
-              key={LAYER.id}
-              className="bg-red flex h-full flex-col justify-center space-y-4 pr-20"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                ease: 'linear',
-                duration: 0.1,
-              }}
-            >
-              <h3 className="font-display text-5xl">{LAYER.title}</h3>
-              <>{LAYER.content}</>
-            </motion.div>
-          </AnimatePresence>
+    <motion.section
+      key="layers"
+      className="absolute flex h-full w-full items-center justify-center overflow-hidden pt-20 text-navy"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: STEP_DURATION * 0.5 }}
+    >
+      <Wrapper>
+        <div className="grid h-small-screen grid-cols-12 gap-20">
+          <div
+            className={cn({
+              'relative col-span-5': true,
+              'before:absolute before:top-0 before:right-full before:h-full before:w-[50vw]': true,
+              [LAYER.backgroundColor]: true,
+            })}
+          >
+            <FadeY className="h-full">
+              <AnimatePresence>
+                <motion.div
+                  key={LAYER.id}
+                  className="flex h-full flex-col justify-center space-y-10 pr-20"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{
+                    ease: 'linear',
+                    duration: 0.1,
+                  }}
+                >
+                  <h3 className="font-display text-5xl">{LAYER.title}</h3>
+                  <>{LAYER.content}</>
+                </motion.div>
+              </AnimatePresence>
+            </FadeY>
 
-          <button className="absolute bottom-9 mx-auto mb-5 flex flex-col items-center space-y-4 rounded-full">
-            <Icon icon={ARROW_DOWN_SVG} className="h-4 w-4 animate-bounce" />
-          </button>
-        </div>
+            <button className="absolute bottom-20 mx-auto mb-5 flex flex-col items-center space-y-4 rounded-full">
+              <Icon icon={ARROW_DOWN_SVG} className="h-4 w-4 animate-bounce" />
+            </button>
+          </div>
 
-        <div className="col-span-7 flex flex-col items-center justify-center">
-          <div className="relative aspect-square w-full">
-            <Button
-              theme="green"
-              size="base"
-              className="absolute top-[20%] left-0 z-10 uppercase"
-              onClick={() => onClick(PHYSICAL)}
-              selected={layerId === PHYSICAL}
-              unselected={layerId !== 'empty' && layerId !== PHYSICAL}
-            >
-              Physical Geography
-            </Button>
-            <Button
-              theme="yellow"
-              size="base"
-              className="absolute top-[12%] right-[10%] z-10 uppercase"
-              onClick={() => onClick(SOCIO)}
-              selected={layerId === SOCIO}
-              unselected={layerId !== 'empty' && layerId !== SOCIO}
-            >
-              Socioeconomic Influence
-            </Button>
-            <Button
-              theme="red"
-              size="base"
-              className="absolute bottom-[20%] left-[25%] uppercase"
-              onClick={() => onClick(MANAGEMENT)}
-              selected={layerId === MANAGEMENT}
-              unselected={layerId !== 'empty' && layerId !== MANAGEMENT}
-            >
-              Management Patterns
-            </Button>
+          <div className="col-span-7 flex flex-col items-center justify-center">
+            <div className="relative aspect-square w-full">
+              <Button
+                theme="green"
+                size="base"
+                className="absolute top-[20%] left-0 z-10 uppercase"
+                onClick={() => onClick(PHYSICAL)}
+                selected={layerId === PHYSICAL}
+                unselected={layerId !== 'empty' && layerId !== PHYSICAL}
+              >
+                Physical Geography
+              </Button>
+              <Button
+                theme="yellow"
+                size="base"
+                className="absolute top-[12%] right-[10%] z-10 uppercase"
+                onClick={() => onClick(SOCIO)}
+                selected={layerId === SOCIO}
+                unselected={layerId !== 'empty' && layerId !== SOCIO}
+              >
+                Socioeconomic Influence
+              </Button>
+              <Button
+                theme="red"
+                size="base"
+                className="absolute bottom-[20%] left-[25%] uppercase"
+                onClick={() => onClick(MANAGEMENT)}
+                selected={layerId === MANAGEMENT}
+                unselected={layerId !== 'empty' && layerId !== MANAGEMENT}
+              >
+                Management Patterns
+              </Button>
 
-            <AnimatePresence>
-              <motion.div
-                key="empty-0"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{
-                  ease: 'linear',
-                  duration: 0.5,
-                }}
-                className="absolute h-full w-full bg-no-repeat"
-                style={{
-                  backgroundImage: `url(${LAYERS[0].imageUrl})`,
-                  backgroundSize: '100% auto',
-                }}
-              />
+              <AnimatePresence>
+                <motion.div
+                  key="empty-0"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{
+                    ease: 'linear',
+                    duration: 0.5,
+                  }}
+                  className="absolute h-full w-full bg-no-repeat"
+                  style={{
+                    backgroundImage: `url(${LAYERS[0].imageUrl})`,
+                    backgroundSize: '100% auto',
+                  }}
+                />
 
-              {FILTERED_LAYERS.map((l) => {
-                return (
-                  <motion.div
-                    key={l.id}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{
-                      ease: 'linear',
-                      duration: 0.5,
-                    }}
-                    className="absolute h-full w-full bg-no-repeat"
-                    style={{
-                      backgroundImage: `url(${l.imageUrl})`,
-                      backgroundSize: '100% auto',
-                    }}
-                  />
-                );
-              })}
-            </AnimatePresence>
+                {FILTERED_LAYERS.map((l) => {
+                  return (
+                    <motion.div
+                      key={l.id}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      transition={{
+                        ease: 'linear',
+                        duration: 0.5,
+                      }}
+                      className="absolute h-full w-full bg-no-repeat"
+                      style={{
+                        backgroundImage: `url(${l.imageUrl})`,
+                        backgroundSize: '100% auto',
+                      }}
+                    />
+                  );
+                })}
+              </AnimatePresence>
+            </div>
           </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </motion.section>
   );
 };
 
