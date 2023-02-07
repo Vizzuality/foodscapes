ebd5a5200378de8c1e3e39292288eb47576d2530
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-02-07 15:41:00 +0100

    Home: 3d model camera

--- client/src/containers/home/layers/charts/camera/component.tsx
+++ client/src/containers/home/layers/charts/camera/component.tsx
@@ -4,24 +4,27 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { useRecoilValue } from 'recoil';
 import { Vector3 } from 'three';
 
-const vec3 = new Vector3(1, 4, 4);
+const pos = new Vector3(1, 4, 4);
 
 const POSITIONS = {
   0: [1, 4, 4],
-  1: [1, 5, 3],
-  2: [2, 3, 4],
+  1: [4, 3, 2],
+  2: [1, 3, 1],
   3: [3, 4, 4],
   4: [3, 4, 4],
 };
-
 const Camera = () => {
   const { camera } = useThree();
   const step = useRecoilValue(stepAtom);
 
   useFrame(() => {
-    const [x, y, z] = POSITIONS[step];
-    vec3.set(x, y, z);
-    camera.position.lerp(vec3, 0.075);
+    const [x, y, z] = POSITIONS[step] || POSITIONS[0];
+
+    // Move camera to position
+    pos.set(x, y, z);
+    camera.position.lerp(pos, 0.1);
+
+    // Look at target
     camera.lookAt(0, 0, 0);
   });
 
