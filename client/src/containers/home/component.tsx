125a8d13e4688f4c2cb055d12e29e5b51d43f6af
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-02-09 10:31:24 +0100

    Home: 3d model fullscreen

--- client/src/containers/home/component.tsx
+++ client/src/containers/home/component.tsx
@@ -57,8 +57,8 @@ const Home = () => {
           </div>
           <div className="sticky top-0 z-0 col-span-5 col-start-7 h-small-screen">
             <AnimatePresence>
-              {/* {(step === 0 || step === 1 || step === 2 || step === 3 || step === 4) && ( */}
-              {step === -1 && (
+              {(step === 0 || step === 1 || step === 2 || step === 3 || step === 4) && (
+                // {step === -1 && (
                 <FadeY key="layers-chart">
                   <LayersChart />
                 </FadeY>
