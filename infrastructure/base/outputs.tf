47ad3888b818df3066f162573bdfbf079bf053c2
andrea rota <andrea.rota@vizzuality.com>
2023-02-14 14:00:24 +0000

    add scaffolding of base Terraform setup

---
+++ infrastructure/base/outputs.tf
@@ -0,0 +1,21 @@
+# Output values which can be referenced in other repos
+output "account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "ID of AWS account"
+}
+
+output "vpc_id" {
+  value = module.vpc.id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "foodscapes_container_registry_url" {
+  value = module.foodscapes_container_registry.container_registry_url
+}
