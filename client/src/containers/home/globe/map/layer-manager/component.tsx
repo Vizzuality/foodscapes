58557db0f905560bf49104859558448bb9323a9f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-03-13 10:44:11 +0100

    Remove layer-manager dependencies

--- client/src/containers/home/globe/map/layer-manager/component.tsx
+++ client/src/containers/home/globe/map/layer-manager/component.tsx
@@ -1,17 +1,10 @@
-import { useMap } from 'react-map-gl';
-
-import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
-import { LayerManager } from '@vizzuality/layer-manager-react';
-
 import FoodscapesLayer from 'containers/datasets/foodscapes/layer';
 
 const LayerManagerContainer = () => {
-  const { current: map } = useMap();
-
   return (
-    <LayerManager map={map.getMap()} plugin={PluginMapboxGl}>
+    <>
       <FoodscapesLayer />
-    </LayerManager>
+    </>
   );
 };
 
