251433abfecef40502ededcb4ed8a983bc3e6601
Maria Sola <maria.sola@vizzuality.com>
2023-03-14 17:16:45 +0100

    about page fixes

--- client/src/containers/about/component.tsx
+++ client/src/containers/about/component.tsx
@@ -10,12 +10,16 @@ const About = () => {
   return (
     <>
       <Wrapper>
-        <div className="grid grid-cols-12 gap-6">
-          <div className="col-span-6 xl:col-span-5 xl:col-start-2">
-            <div className="flex h-small-screen flex-col justify-center space-y-10 ">
+        <div className="space-y-10 py-24">
+          <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-6 xl:col-span-5 xl:col-start-2">
               <h2 className="font-display text-5xl">About Foodscapes.</h2>
+            </div>
+          </div>
 
-              <div className="space-y-4">
+          <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-6 xl:col-span-5 xl:col-start-2">
+              <div className="flex flex-col justify-center space-y-4">
                 <p className="font-light">
                   [Placeholder text] Foodscapes is a Lorem ipsum dolor sit amet, consectetur
                   adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
@@ -35,17 +39,16 @@ const About = () => {
                 <Icon icon={TNC_SVG} className="h-10 w-36" />
               </div>
             </div>
-          </div>
 
-          <div className="col-span-5 col-start-8 h-small-screen">
-            <div className="flex h-full flex-col items-end justify-center">
-              <Image
-                width={456}
-                height={280}
-                src="/images/about/about.jpg"
-                alt="About field image"
-                className="pt-36"
-              />
+            <div className="col-span-5 col-start-8">
+              <div className="flex h-full flex-col items-end justify-center">
+                <Image
+                  width={456}
+                  height={280}
+                  src="/images/about/about.jpg"
+                  alt="About field image"
+                />
+              </div>
             </div>
           </div>
         </div>
