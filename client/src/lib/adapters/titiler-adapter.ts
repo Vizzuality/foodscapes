5f683ac02a3080d8bf1c6f298bfcd5ad280a1687
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-04-05 09:34:04 +0200

    Titiler adapter

--- client/src/lib/adapters/titiler-adapter.ts
+++ client/src/lib/adapters/titiler-adapter.ts
@@ -12,8 +12,10 @@ export function titilerAdapter(params: TitilerParamsProps = {}) {
   return [
     ...(foodscapes.length ? [foodscapes.map((v) => `(b1==${v})`).join('|')] : []),
     ...(intensities.length ? [intensities.map((v) => `(b3==${v})`).join('|')] : []),
-    ...(crops.length ? [crops.map((v) => `(b2==${v})`).join('|')] : []),
+    ...(crops.length ? [crops.map((v) => `(b4==${v})`).join('|')] : []),
     ...(country ? [`(b31==${location})`] : []),
     ...(province ? [`(b32==${location})`] : []),
-  ].join('&');
+  ]
+    .map((v) => `(${v})`)
+    .join('&');
 }
