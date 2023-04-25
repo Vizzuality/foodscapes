e7dccf22a66c15bbb62f0dcde66e5d12ede33d67
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-04-25 13:46:30 +0200

    Download data

--- client/src/containers/widget/header/index.tsx
+++ client/src/containers/widget/header/index.tsx
@@ -2,22 +2,31 @@ import { useCallback } from 'react';
 
 import { layersAtom } from 'store/explore-map';
 
+import { TooltipPortal } from '@radix-ui/react-tooltip';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { Dataset } from 'types/datasets';
 
+import { useDownloadData } from 'hooks/data';
+
+import Icon from 'components/icon';
 import Switch from 'components/ui/switch';
+import { Tooltip, TooltipArrow, TooltipContent, TooltipTrigger } from 'components/ui/tooltip';
+
+import DOWNLOAD_SVG from 'svgs/ui/download.svg?sprite';
 
 export interface WidgetHeaderProps {
   title: string;
   dataset: Dataset;
 }
 
-const WidgetHeader = ({ title, dataset }) => {
+const WidgetHeader = ({ title, dataset }: WidgetHeaderProps) => {
   const { id } = dataset;
 
   const layers = useRecoilValue(layersAtom);
   const setLayers = useSetRecoilState(layersAtom);
+
+  const downloadMutation = useDownloadData();
 
   const handleToggleLayer = useCallback(() => {
     const lys = [...layers];
@@ -33,11 +42,70 @@ const WidgetHeader = ({ title, dataset }) => {
     setLayers(lys);
   }, [id, layers, setLayers]);
 
+  const handleDownload = useCallback(() => {
+    downloadMutation.mutate(
+      { sql: dataset.widget.sql },
+      {
+        onSuccess: (data) => {
+          const url = window.URL.createObjectURL(new Blob([data]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', `${dataset.label}.csv`);
+          document.body.appendChild(link);
+          link.click();
+          link.remove();
+        },
+        onError: (error) => console.error(error),
+      }
+    );
+  }, [dataset, downloadMutation]);
+
   return (
     <header className="flex items-center justify-between space-x-5">
       <h3 className="font-display text-2xl">{title}</h3>
 
-      <Switch checked={layers.includes(id)} onCheckedChange={handleToggleLayer} />
+      <div className="flex items-center space-x-2">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              type="button"
+              className="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full text-navy-500 transition-all hover:bg-navy-200"
+              onClick={handleDownload}
+            >
+              <Icon icon={DOWNLOAD_SVG} className="h-5 w-5" />
+            </button>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent
+              side="top"
+              align="center"
+              className="rounded-none border-navy-500 bg-navy-500"
+            >
+              <div className="text-xxs text-white">Download data</div>
+
+              <TooltipArrow className="fill-navy-500" width={10} height={5} />
+            </TooltipContent>
+          </TooltipPortal>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger>
+            <Switch checked={layers.includes(id)} onCheckedChange={handleToggleLayer} />
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent
+              side="top"
+              align="center"
+              className="rounded-none border-navy-500 bg-navy-500"
+            >
+              <div className="text-xxs text-white">
+                {layers.includes(id) ? 'Remove from map' : 'Add to map'}
+              </div>
+
+              <TooltipArrow className="fill-navy-500" width={10} height={5} />
+            </TooltipContent>
+          </TooltipPortal>
+        </Tooltip>
+      </div>
     </header>
   );
 };
