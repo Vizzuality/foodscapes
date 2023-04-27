5d5804eb3189a10abc1f9737b73f7c439018b67f
andrea rota <andrea.rota@vizzuality.com>
2023-04-27 14:00:31 +0100

    import foodscapes metadata

---
+++ data/import/foodscapes/foodscapes_metadata-setup.sql
@@ -0,0 +1,77 @@
+.load mod_spatialite
+select InitSpatialMetadata();
+
+CREATE TABLE crop_groups (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [color] text
+);
+
+CREATE TABLE crops (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [color] text,
+  [parent_id] integer references crop_group_labels(value)
+);
+
+CREATE TABLE soil_groups (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [color] text
+);
+
+CREATE TABLE meta_foodscapes (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [color] text,
+  [parent_id] integer references soil_group_labels(value)
+);
+
+CREATE TABLE intensity_groups (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [color] text
+);
+
+CREATE TABLE layers (
+  [id] integer,
+  [section] text,
+  [sub_section] text,
+  [indicator] text,
+  [type] text,
+  [unit] text,
+  [cog_band] integer,
+  [widget_column] text,
+  [legend] text null,
+  [file_name] text
+);
+
+CREATE TABLE countries (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [iso] text,
+  [geometry] text,
+  [bbox] text
+);
+
+SELECT AddGeometryColumn('countries', 'geometry_geom', 4326, 'MULTIPOLYGON');
+SELECT AddGeometryColumn('countries', 'bbox_geom', 4326, 'MULTIPOLYGON');
+
+CREATE TABLE provinces (
+  [id] integer,
+  [value] integer,
+  [label] text,
+  [parent_id] integer,
+  [iso] text,
+  [geometry] text,
+  [bbox] text
+);
+
+SELECT AddGeometryColumn('provinces', 'geometry_geom', 4326, 'MULTIPOLYGON');
+SELECT AddGeometryColumn('provinces', 'bbox_geom', 4326, 'MULTIPOLYGON');
