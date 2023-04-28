86354e8ceccfe06aa2ae3b64e168869c4206cfb6
andrea rota <andrea.rota@vizzuality.com>
2023-04-28 15:11:25 +0100

    allow to select source CSVs via date and revision CLI params

--- data/import/foodscapes/foodscapes_csv-to-sqlite.sh
+++ data/import/foodscapes/foodscapes_csv-to-sqlite.sh
@@ -1,25 +1,53 @@
 #!/bin/sh
 
-# Check if user has specified a -f flag; if so delete the database first
-if [ "$1" = "-f" ]; then
-    rm dest/foodscapes.db
+while [ $# -gt 0 ]
+do
+    case "$1" in
+      -f|--force)
+        FORCE=true
+        shift
+        ;;
+      -d|--date)
+        DATE="$2"
+        shift
+        shift
+        ;;
+      -r|--revision)
+        REVISION="$2"
+        shift
+        shift
+        ;;
+    esac
+done
+
+# exit with error if DATE or REVISION are not set
+if [ -z "$DATE" ] || [ -z "$REVISION" ]
+then
+  echo "Please specify both a date (YYYYMMDD) and revision number (XY)"
+  exit 1
+fi
+
+# delete the database first, if FORCE is true
+if [ "$FORCE" = true ]
+then
+  rm -f dest/foodscapes.db
 fi
 
 sqlite3 dest/foodscapes.db < foodscapes_sqlite-db-setup.sql
-cat source/foodscapes_filter_table.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' data"
+cat source/foodscapes_filter_table_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' data"
 
 sqlite3 dest/foodscapes.db < foodscapes_metadata-setup.sql
-cat source/label-data/crop_group_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' crop_groups"
-cat source/label-data/crop_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' crops"
+cat source/metadata/crop_group_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' crop_groups"
+cat source/metadata/crop_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' crops"
 
-cat source/label-data/soil_group_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' soil_groups"
-cat source/label-data/foodscapes_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' foodscapes"
+cat source/metadata/soil_group_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' soil_groups"
+cat source/metadata/foodscapes_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' foodscapes"
 
-cat source/label-data/intensity_group_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' intensity_groups"
+cat source/metadata/intensity_group_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' intensity_groups"
 
-cat source/label-data/layers_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' layers"
+cat source/metadata/layers_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' layers"
 
-cat source/label-data/countries_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' countries"
-cat source/label-data/provinces_legend_20230421_00.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' provinces"
+cat source/metadata/countries_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' countries"
+cat source/metadata/provinces_legend_${DATE}_${REVISION}.csv | sqlite3 -csv dest/foodscapes.db ".import --skip 1 '| cat -' provinces"
 
 sqlite3 dest/foodscapes.db < foodscapes_metadata-cleanup.sql
