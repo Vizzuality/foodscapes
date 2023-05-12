ec7d237f9db287032c28c197a0693198d8a7ba64
andrea rota <andrea.rota@vizzuality.com>
2023-05-12 11:12:45 +0100

    pre-render GeoJSON columns
    Also clean up column names for consistency, and remove imported WKT text
    column after use.

--- data/import/foodscapes/foodscapes_metadata-cleanup.sql
+++ data/import/foodscapes/foodscapes_metadata-cleanup.sql
@@ -21,19 +21,59 @@ ALTER TABLE layers
 ALTER TABLE countries
   DROP COLUMN id;
 
-SELECT AddGeometryColumn('countries', 'geometry_geom', 4326, 'MULTIPOLYGON', 'XY');
+ALTER TABLE countries
+  RENAME COLUMN "geometry" to "geometry_wkt";
+
+ALTER TABLE countries
+  ADD COLUMN bbox_geojson text;
+
+ALTER TABLE countries
+  ADD COLUMN geometry_geojson text;
+
+SELECT AddGeometryColumn('countries', 'geometry', 4326, 'MULTIPOLYGON', 'XY');
 
 UPDATE countries
   SET
-    geometry_geom = GeomFromText(geometry, 4326);
+    geometry = GeomFromText(geometry_wkt, 4326);
+
+UPDATE countries
+  SET
+    bbox_geojson = AsGeoJSON(Envelope(geometry));
+
+UPDATE countries
+  SET
+    geometry_geojson = AsGeoJSON(geometry);
+
+ALTER TABLE countries
+  DROP COLUMN geometry_wkt;
 
 ALTER TABLE provinces
   DROP COLUMN id;
 
-SELECT AddGeometryColumn('provinces', 'geometry_geom', 4326, 'MULTIPOLYGON', 'XY');
+ALTER TABLE provinces
+  RENAME COLUMN "geometry" to "geometry_wkt";
+
+ALTER TABLE provinces
+  ADD COLUMN bbox_geojson text;
+
+ALTER TABLE provinces
+  ADD COLUMN geometry_geojson text;
+
+SELECT AddGeometryColumn('provinces', 'geometry', 4326, 'MULTIPOLYGON', 'XY');
 
 UPDATE provinces
   SET
-    geometry_geom = GeomFromText(geometry, 4326);
+    geometry = GeomFromText(geometry_wkt, 4326);
+
+UPDATE provinces
+  SET
+    bbox_geojson = AsGeoJSON(Envelope(geometry));
+
+UPDATE provinces
+  SET
+    geometry_geojson = AsGeoJSON(geometry);
+
+ALTER TABLE provinces
+  DROP COLUMN geometry_wkt;
 
 VACUUM;
