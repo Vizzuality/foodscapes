ed75a0d4e42aa1f842d2fd8048afe830762d6eab
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-05-17 13:06:53 +0200

    Metadata: styles

--- client/src/layouts/mdx/component.tsx
+++ client/src/layouts/mdx/component.tsx
@@ -6,7 +6,7 @@ const LayoutMdx: React.FC<LayoutMdxProps> = (props: LayoutMdxProps) => {
   const { children } = props;
 
   return (
-    <article className="prose">
+    <article className="prose flex grow flex-col overflow-auto p-6 prose-headings:font-display prose-p:mb-0">
       {/* Content */}
       {children}
     </article>
