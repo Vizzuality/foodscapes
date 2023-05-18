9241eaebed0565f6462170bb3a806c2317954f28
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-05-18 13:00:35 +0100

    Remove ternaries from CaseStudiesList

--- client/src/containers/case-studies/list/component.tsx
+++ client/src/containers/case-studies/list/component.tsx
@@ -1,32 +1,23 @@
 import { useCaseStudies } from 'hooks/case-studies';
 
+import { Skeleton } from 'components/ui/skeleton';
+
 import CaseStudiesListItem from '../list-item/component';
 
-const NUM_LOADING_SKELETON_ENTRIES = 6;
-
 const CaseStudiesList = () => {
-  const { data: caseStudies, isFetching, isFetched, isError } = useCaseStudies();
+  const { data: caseStudies, isFetching, isFetched, isError, isPlaceholderData } = useCaseStudies();
 
   return (
     <section className="space-y-4 py-10">
-      {isError ? (
+      {isPlaceholderData && <Skeleton className="h-20 w-full" />}
+      {isError && isFetched && !isFetching && (
         <div className="text-center">Oops!! Something went wrong</div>
-      ) : (
+      )}
+      {!isPlaceholderData && !isError && (
         <div className="grid grid-cols-2 gap-x-4 gap-y-10">
-          {isFetched && (
-            <>
-              {caseStudies.map((caseStudy) => (
-                <CaseStudiesListItem key={caseStudy.id} caseStudy={caseStudy} />
-              ))}
-            </>
-          )}
-          {isFetching && (
-            <>
-              {[...Array(NUM_LOADING_SKELETON_ENTRIES)].map((_, idx) => (
-                <CaseStudiesListItem key={idx} />
-              ))}
-            </>
-          )}
+          {caseStudies.map((caseStudy) => (
+            <CaseStudiesListItem key={caseStudy.id} caseStudy={caseStudy} />
+          ))}
         </div>
       )}
     </section>
