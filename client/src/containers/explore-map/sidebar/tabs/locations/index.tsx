1518b2399ec15f3cb0d84b0945369497e555f5aa
Maria Sola <maria.sola@vizzuality.com>
2023-05-18 15:49:30 +0200

    flex grow

--- client/src/containers/explore-map/sidebar/tabs/locations/index.tsx
+++ client/src/containers/explore-map/sidebar/tabs/locations/index.tsx
@@ -6,31 +6,33 @@ import LocationRankingWidget from 'containers/datasets/location/widget';
 
 const LocationsSidebar = forwardRef(() => {
   return (
-    <section>
-      <header>
-        <div className="relative z-0 px-20 pt-36">
-          <h2 className="relative z-10 border-b border-navy-500/30 pb-6 text-xl font-medium">
-            Locations
-          </h2>
-          <h3 className="relative z-10 pt-6 font-display text-5xl">Foodscapes in Place</h3>
-          <div className="absolute top-0 left-0 z-0 h-[calc(100%_-_theme(space.8))] w-full bg-blue-500" />
-        </div>
-        <div className="space-y-4 px-20 pt-4 pb-10">
-          <p>
-            To accelarate a global food system transformation we can take inspiration from the
-            nature-based solutions that would work in one spefic area and extrapolate these to
-            similar foodscapes worldwide.
-          </p>
-          {/* <p>
+    <section className="flex grow flex-col justify-between">
+      <div>
+        <header>
+          <div className="relative z-0 px-20 pt-36">
+            <h2 className="relative z-10 border-b border-navy-500/30 pb-6 text-xl font-medium">
+              Locations
+            </h2>
+            <h3 className="relative z-10 pt-6 font-display text-5xl">Foodscapes in Place</h3>
+            <div className="absolute top-0 left-0 z-0 h-[calc(100%_-_theme(space.8))] w-full bg-blue-500" />
+          </div>
+          <div className="space-y-4 px-20 pt-4 pb-10">
+            <p>
+              To accelarate a global food system transformation we can take inspiration from the
+              nature-based solutions that would work in one spefic area and extrapolate these to
+              similar foodscapes worldwide.
+            </p>
+            {/* <p>
             This section gives the opportunity to discover the state of foodscapes around the globe.
           </p> */}
-        </div>
-      </header>
+          </div>
+        </header>
 
-      <div className="w-full px-20">
-        {/* <div className="border-t border-navy-500/30"> */}
-        <LocationRankingWidget />
-        {/* </div> */}
+        <div className="w-full px-20">
+          {/* <div className="border-t border-navy-500/30"> */}
+          <LocationRankingWidget />
+          {/* </div> */}
+        </div>
       </div>
 
       <div>
