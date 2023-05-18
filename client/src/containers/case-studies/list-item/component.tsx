b5d15d410d7caf9e31e521c2a6311fc84e552b42
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-05-18 12:57:02 +0100

    Remove caseStudy check in CaseStudiesListItem; make it mandatory

--- client/src/containers/case-studies/list-item/component.tsx
+++ client/src/containers/case-studies/list-item/component.tsx
@@ -2,34 +2,24 @@ import Image from 'next/image';
 
 import { CaseStudy } from 'types/case-studies';
 
-import { Skeleton } from 'components/ui/skeleton';
-
 interface CaseStudiesListItemProps {
-  caseStudy?: CaseStudy;
+  caseStudy: CaseStudy;
 }
 
 const CaseStudiesListItem = ({ caseStudy }: CaseStudiesListItemProps) => {
   return (
     <div className="flex flex-col text-center">
-      <span className="mb-4 px-4 font-display">
-        {caseStudy ? <>{caseStudy.title}</> : <Skeleton className="h-6 w-full" />}
-      </span>
+      <span className="mb-4 px-4 font-display">{caseStudy.title}</span>
       <span className="relative block h-48 overflow-hidden">
-        {caseStudy ? (
-          <Image
-            className="duration-300 ease-in hover:scale-110"
-            src={caseStudy.image}
-            alt={caseStudy.title}
-            fill
-            priority
-          />
-        ) : (
-          <Skeleton className="h-48 w-full" />
-        )}
+        <Image
+          className="duration-300 ease-in hover:scale-110"
+          src={caseStudy.image}
+          alt={caseStudy.title}
+          fill
+          priority
+        />
       </span>
-      <span className="mt-4 text-xs">
-        {caseStudy ? <>{caseStudy.description}</> : <Skeleton className="h-6 w-full" />}
-      </span>
+      <span className="mt-4 text-xs">{caseStudy.description}</span>
     </div>
   );
 };
