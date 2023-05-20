ca74fb1dd8bfdb74d3f6236c8ea5ec1f4d2af7fe
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-05-20 23:18:44 +0200

    Crops: api

--- client/src/hooks/crops/index.ts
+++ client/src/hooks/crops/index.ts
@@ -1,39 +1,17 @@
 import { useMemo } from 'react';
 
-import { datasetteAdapter } from 'lib/adapters/datasette-adapter';
-
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { group } from 'd3-array';
-import squel from 'squel';
 
 import { Crop, CropGroup } from 'types/crops';
 
 import API from 'services/api';
 
-const crops = squel.select().from('crops');
-const cropsGroups = squel.select().from('crop_groups');
-
-const SQL = squel
-  .select()
-  .from(crops, 'f')
-  .left_join(cropsGroups, 's', 'f.parent_id = s.value')
-  .field('f.value', 'id')
-  .field('f.value')
-  .field('f.label')
-  .field('f.color')
-  .field('f.parent_id', 'parentId')
-  .field('s.label', 'parentLabel')
-  .field('s.color', 'parentColor');
-
 export function useCrops(queryOptions: UseQueryOptions<Crop[], unknown> = {}) {
   const fetchCrops = () => {
     return API.request({
       method: 'GET',
-      url: '/foodscapes.json',
-      params: datasetteAdapter({
-        sql: SQL,
-        shape: 'array',
-      }),
+      url: '/crops',
     }).then((response) => response.data);
   };
 
@@ -66,11 +44,7 @@ export function useCropsGroups(queryOptions: UseQueryOptions<Crop[], unknown> = 
   const fetchCrops = () => {
     return API.request({
       method: 'GET',
-      url: '/foodscapes.json',
-      params: datasetteAdapter({
-        sql: SQL,
-        shape: 'array',
-      }),
+      url: '/crops',
     }).then((response) => response.data);
   };
 
