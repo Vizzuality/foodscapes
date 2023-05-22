0c0ab7fdb2f9cf2ca02dd39fb6656e7f3390c7f6
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-05-22 09:40:31 +0200

    Climate/Pollution risk: download

--- client/src/pages/api/climate-risks/download.ts
+++ client/src/pages/api/climate-risks/download.ts
@@ -15,12 +15,32 @@ const fetch = async () => {
     KNEX
       //
       .select(KNEX.raw("'risk' AS id"))
-      .select(KNEX.raw('SUM(CASE WHEN climate_risk = 1 THEN pixel_count ELSE 0 END) AS value'))
+      .select(KNEX.raw('SUM(CASE WHEN d.climate_risk = 1 THEN pixel_count ELSE 0 END) AS value'))
+      .select(
+        KNEX.raw(
+          'SUM(CASE WHEN d.climate_risk = 1 THEN d.pixel_count * 3086.9136 ELSE 0 END) AS ha'
+        )
+      )
+      .select(
+        KNEX.raw(
+          '(SUM(CASE WHEN d.climate_risk = 1 THEN d.pixel_count ELSE 0 END) / (SELECT SUM(d.pixel_count) * 1.0)) * 100 as percentage'
+        )
+      )
       .from('data AS d'),
     KNEX
       //
       .select(KNEX.raw("'not_risk' AS id"))
       .select(KNEX.raw('SUM(CASE WHEN climate_risk = 0 THEN pixel_count ELSE 0 END) AS value'))
+      .select(
+        KNEX.raw(
+          'SUM(CASE WHEN d.climate_risk = 0 THEN d.pixel_count * 3086.9136 ELSE 0 END) AS ha'
+        )
+      )
+      .select(
+        KNEX.raw(
+          '(SUM(CASE WHEN d.climate_risk = 0 THEN d.pixel_count ELSE 0 END) / (SELECT SUM(d.pixel_count) * 1.0)) * 100 as percentage'
+        )
+      )
       .from('data AS d'),
   ];
 
