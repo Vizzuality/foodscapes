4741fcc9d72c97e7e800c938cc21024b9b2ae944
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-05-29 11:38:47 +0200

    Opportunities: popup

--- client/src/containers/datasets/soil-healths/popup.tsx
+++ client/src/containers/datasets/soil-healths/popup.tsx
@@ -8,7 +8,7 @@ import { SoilHealth } from 'types/soil-healths';
 
 import { noPointData, usePointData } from 'hooks/data';
 import { useSoilHealths } from 'hooks/soil-healths';
-import { useIsLoading } from 'hooks/utils';
+import { formatHA, formatPercentage, useIsLoading } from 'hooks/utils';
 
 import { Skeleton } from 'components/ui/skeleton';
 
@@ -18,13 +18,6 @@ interface SoilHealthsPopupProps {
 }
 
 const SoilHealthsPopup = ({ latLng, settings }: SoilHealthsPopupProps) => {
-  const { format } = new Intl.NumberFormat('en-US', {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-    minimumSignificantDigits: 1,
-    maximumSignificantDigits: 3,
-  });
-
   const f = useSoilHealths();
 
   const p = usePointData(latLng, {
@@ -71,7 +64,21 @@ const SoilHealthsPopup = ({ latLng, settings }: SoilHealthsPopupProps) => {
         {isFetched && (
           <>
             {!DATA && <h3 className="text-sm font-light">No data</h3>}
-            {!!DATA && DATA > 1 && <h3 className="text-sm font-light">{format(DATA)} ha</h3>}
+            {!!DATA && DATA > 1 && (
+              <h3 className="text-sm font-light">
+                {`
+                ${formatHA(DATA, {
+                  notation: 'standard',
+                  maximumFractionDigits: 0,
+                })}
+                |
+                ${formatPercentage(DATA / 3086.9136, {
+                  notation: 'standard',
+                  maximumFractionDigits: 2,
+                })}
+                `}
+              </h3>
+            )}
             {!!DATA && DATA < 1 && <h3 className="text-sm font-light">{'< 1 ha'}</h3>}
           </>
         )}
