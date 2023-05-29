e84d6f3a67892921c1541338a4bc3dc037f07be0
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-05-29 12:30:55 +0100

    Update Case Studies hooks to fetch case studies from the API and merge local data

--- client/src/hooks/case-studies/index.ts
+++ client/src/hooks/case-studies/index.ts
@@ -1,72 +1,51 @@
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import CaseStudiesInfo from 'data/case-studies';
 
 import { CaseStudy } from 'types/case-studies';
 
-const MOCKED_DATA_JSON = [
-  {
-    id: 0,
-    title: 'Argentina Gran Chaco',
-    description: 'Halt biodiversity loss through mixed land use',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut purus odio.',
-    image: '/images/stories/argentina-gran-chaco/granchaco-1.jpg',
-    geojson: { type: 'FeatureCollection', features: [] },
-    bbox: [19.28714942900018, 39.657288000000165, 21.049087524000015, 42.65844345100004],
-  },
-  {
-    id: 1,
-    title: 'Arkhangai',
-    description: 'Community-based conservation to protect range lands',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut purus odio.',
-    image: '/images/stories/argentina-gran-chaco/granchaco-2.jpg',
-    geojson: { type: 'FeatureCollection', features: [] },
-    bbox: [19.28714942900018, 39.657288000000165, 21.049087524000015, 42.65844345100004],
-  },
-  {
-    id: 2,
-    title: 'Central New Zealand Aquaculture',
-    description: 'Aquaculture diversification for resilience',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut purus odio.',
-    image: '/images/stories/argentina-gran-chaco/granchaco-3.jpg',
-    geojson: { type: 'FeatureCollection', features: [] },
-    bbox: [19.28714942900018, 39.657288000000165, 21.049087524000015, 42.65844345100004],
-  },
-  {
-    id: 3,
-    title: 'Chesapeake Bay Watershed',
-    description: 'Restore natural habitats to enhance success of nutrient reductions',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut purus odio.',
-    image: '/images/stories/argentina-gran-chaco/granchaco-4.jpg',
-    geojson: { type: 'FeatureCollection', features: [] },
-    bbox: [19.28714942900018, 39.657288000000165, 21.049087524000015, 42.65844345100004],
-  },
-  {
-    id: 4,
-    title: 'East Kalimantan',
-    description: 'Protect and enhance habitat through adaptive land use',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut purus odio.',
-    image: '/images/stories/argentina-gran-chaco/granchaco-1.jpg',
-    geojson: { type: 'FeatureCollection', features: [] },
-    bbox: [19.28714942900018, 39.657288000000165, 21.049087524000015, 42.65844345100004],
-  },
-  {
-    id: 5,
-    title: 'Granada',
-    description: 'Ensure climate resilience by promoting return to traditionally used practices',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut purus odio.',
-    image: '/images/stories/argentina-gran-chaco/granchaco-2.jpg',
-    geojson: { type: 'FeatureCollection', features: [] },
-    bbox: [19.28714942900018, 39.657288000000165, 21.049087524000015, 42.65844345100004],
-  },
-] satisfies CaseStudy[];
+import API from 'services/api';
+
+function addInfoToCaseStudy(caseStudy) {
+  const caseStudyInfo = CaseStudiesInfo[caseStudy?.slug];
+
+  return {
+    ...caseStudy,
+    ...(caseStudyInfo && {
+      ...caseStudyInfo?.data,
+      content: caseStudyInfo.content,
+    }),
+  };
+}
 
 export function useCaseStudies(queryOptions: UseQueryOptions<CaseStudy[], unknown> = {}) {
   const fetchCaseStudies = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(MOCKED_DATA_JSON), 2000);
-    });
+    return API.request({
+      method: 'GET',
+      url: '/case-studies',
+    }).then((response) =>
+      response.data
+        .map((caseStudy) => addInfoToCaseStudy(caseStudy))
+        .filter(({ content }) => !!content)
+    );
   };
 
   const query = useQuery(['case-studies'], fetchCaseStudies, {
+    placeholderData: [],
+    ...queryOptions,
+  });
+
+  return query;
+}
+
+export function useCaseStudy(id, queryOptions: UseQueryOptions<CaseStudy, unknown> = {}) {
+  const fetchCaseStudy = () => {
+    return API.request({
+      method: 'GET',
+      url: `/case-studies/${id}`,
+    }).then((response) => addInfoToCaseStudy(response.data));
+  };
+
+  const query = useQuery(['case-study', id], fetchCaseStudy, {
     placeholderData: [],
     ...queryOptions,
   });
