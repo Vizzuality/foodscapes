add2c84969eed520ac7925f4d51471e3de10508d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-06-08 16:51:23 +0200

    Metadata: little style improvements

--- client/src/layouts/mdx/component.tsx
+++ client/src/layouts/mdx/component.tsx
@@ -1,19 +1,17 @@
 import cn from 'classnames';
 
 type LayoutMdxProps = {
-  type: 'default' | 'case-study';
   children: React.ReactNode;
 };
 
 const LayoutMdx: React.FC<LayoutMdxProps> = (props: LayoutMdxProps) => {
-  const { type = 'default', children } = props;
+  const { children } = props;
 
   return (
     <article
       className={cn({
-        'prose-sm flex grow flex-col prose-headings:font-display prose-h1:mb-0 prose-p:mb-0': true,
-        'overflow-auto p-6': type === 'default',
-        'overflow-visible': type === 'case-study',
+        'prose-sm flex grow flex-col overflow-auto p-6 prose-headings:font-display prose-h1:mb-0 prose-p:mb-0':
+          true,
       })}
     >
       {/* Content */}
