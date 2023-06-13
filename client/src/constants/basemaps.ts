c84f5a3c9fec9d7f4769974f948b879b7e4c17c2
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-06-13 12:14:21 +0200

    Map settings: new account

--- client/src/constants/basemaps.ts
+++ client/src/constants/basemaps.ts
@@ -4,19 +4,46 @@ export const BASEMAPS = [
   {
     label: 'Clean',
     value: 'light',
-    url: 'mapbox://styles/afilatore90/cjuvfwn1heng71ftijvnv2ek6',
-    preview: `https://api.mapbox.com/styles/v1/afilatore90/cjuvfwn1heng71ftijvnv2ek6/static/-100,40,0/80x66?access_token=${env.NEXT_PUBLIC_MAPBOX_API_TOKEN}&logo=false&attribution=false`,
+    url: 'mapbox://styles/foodscapes/clisymb78003t01pncx558l3i',
+    preview: `https://api.mapbox.com/styles/v1/foodscapes/clisymb78003t01pncx558l3i/static/-100,40,0/96x64?access_token=${env.NEXT_PUBLIC_MAPBOX_API_TOKEN}&logo=false&attribution=false`,
   },
   {
     label: 'Satellite',
     value: 'satellite',
-    url: 'mapbox://styles/afilatore90/cjuii5qp93u5i1fm7tzsclkvf',
-    preview: `https://api.mapbox.com/styles/v1/afilatore90/cjuii5qp93u5i1fm7tzsclkvf/static/-100,40,0/80x66?access_token=${env.NEXT_PUBLIC_MAPBOX_API_TOKEN}&logo=false&attribution=false`,
+    url: 'mapbox://styles/foodscapes/cliu3unp700do01p7exykcqt3',
+    preview: `https://api.mapbox.com/styles/v1/foodscapes/cliu3unp700do01p7exykcqt3/static/-100,40,0/96x64?access_token=${env.NEXT_PUBLIC_MAPBOX_API_TOKEN}&logo=false&attribution=false`,
+  },
+];
+
+export const LABELS = [
+  {
+    id: '1059d2b8cfa87b8d894b5373ea556666',
+    name: 'labels-dark',
   },
   {
-    label: 'OpenStreetMap',
-    value: 'open-street-map',
-    url: 'mapbox://styles/afilatore90/cle2kx0wv003601oggtxuquv0',
-    preview: `https://api.mapbox.com/styles/v1/afilatore90/cle2kx0wv003601oggtxuquv0/static/-100,40,0/80x66?access_token=${env.NEXT_PUBLIC_MAPBOX_API_TOKEN}&logo=false&attribution=false`,
+    id: '5924e7eeda116f817dd89f1d8d418721',
+    name: 'labels-light',
+  },
+];
+
+export const BOUNDARIES = [
+  {
+    id: 'ae861f3122c21ad7754e66d3cead38e6',
+    name: 'boundaries-dark',
+  },
+  {
+    id: '31b240eba06a254ade36f1dde6a3c07e',
+    name: 'boundaries-light',
+  },
+];
+
+export const ROADS = [
+  {
+    id: '4e240a8b884456747dcd07d41b4d5543',
+    name: 'roads-dark',
+  },
+  {
+    id: 'edb80ef589e776ec6c2568b2fc6ad74c',
+    name: 'roads-light',
   },
 ];
--- client/src/containers/explore-map/layers/basemaps/component.tsx
+++ client/src/containers/explore-map/layers/basemaps/component.tsx
@@ -7,7 +7,7 @@ const Basemaps = () => {
     <div className="space-y-3 px-6">
       <h3 className="font-display text-lg">Map style</h3>
 
-      <ul className="space-y-2">
+      <ul className="flex space-x-2">
         {BASEMAPS.map((b) => (
           <li key={b.value}>
             <BasemapItem {...b} />
